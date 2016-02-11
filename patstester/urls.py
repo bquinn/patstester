6ec88600c8fb5855d84aa696b7f27a3208f3f7bc
bquinn <brendan@cluefulmedia.com>
2016-02-11 10:46:25 +0000

    Added robots.txt - somehow a web crawler found our URL...

--- patstester/urls.py
+++ patstester/urls.py
@@ -19,5 +19,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     url(r'^', include('main.urls')),
 ]
