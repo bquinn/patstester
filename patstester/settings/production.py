a8fa9873809bdcec2da8b6646b10111a5c3efc44
bquinn <brendan@cluefulmedia.com>
2016-03-16 17:48:50 +0000

    Finally start to use Amazon RDS as a persistent data store rather than the ephemeral sqlite

--- patstester/settings/production.py
+++ patstester/settings/production.py
@@ -12,4 +12,15 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['apitester.pats.org.uk']
 
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'patstester', # 'get_env_variable('PATSTESTER_DB_NAME'),
+    'USER': 'patstester', # get_env_variable('PATSTESTER_DB_USER'),
+    'PASSWORD': 'p4tsT3ster', # get_env_variable('PATSTESTER_DB_PASSWORD'),
+    'HOST': 'patstester-db.cbnfejxauezt.eu-west-1.rds.amazonaws.com', # get_env_variable('PATSTESTEr_DB_HOST'),
+    'PORT': '5432', # get_env_variable('BUBBLYGUIDE_DB_PORT')
+  }
+}
+
 SECRET_KEY = 'ao(i7!wdcnc*%^oex^hir9cazxu_hbq=20d$8iasdfs&&!s0t^h=($'
